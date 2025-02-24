fab5e13753ae8b1e1255a7fb9ba3ed235395f44d
JKs-ui144 <yjlo3521@gmail.com>
2025-02-24 20:52:42 +0900

    Add_JKS
    Add_PlayerController

---
+++ Source/Test_project/Public/GSPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "GSPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEST_PROJECT_API AGSPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
